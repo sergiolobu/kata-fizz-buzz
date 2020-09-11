70af21639e90bffbe0acb21b72170630b8ae6221
sergiolobu <slozanobueno@gmail.com>
2020-09-11 17:48:54 +0200

    refactor FizzBuzzTest.php, add setUp()

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -7,16 +7,22 @@ use App\FizzBuzz;
 
 class FizzBuzzTest extends TestCase
 {
+
+    public function setUp():void
+    {
+        parent::setUp();
+        $this->fizzBuzz = new FizzBuzz();
+    }
+
     /**
      * @test
      */
     public function return_1_when_print_1()
     {
         //Arrange
-        $fizzBuzz = new FizzBuzz();
 
         //Act
-        $result = $fizzBuzz->getResult(1);
+        $result = $this->fizzBuzz->getResult(1);
 
         //Asserts
         $this->assertEquals(1,$result);
@@ -28,10 +34,9 @@ class FizzBuzzTest extends TestCase
     public function return_fizz_when_print_number_divisible_by_3()
     {
         //Arrange
-        $fizzBuzz = new FizzBuzz();
 
         //Act
-        $result = $fizzBuzz->getResult(3);
+        $result = $this->fizzBuzz->getResult(3);
 
         //Asserts
         $this->assertEquals('Fizz',$result);
